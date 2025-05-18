cd07b14e41a98954afee2f56f33d12f76f96e67c
Ludmila <ludamocuk29@gmail.com>
2025-05-19 01:41:27 +0300

    updared

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,1 +1,10 @@
 declare module "modern-normalize";
+// для .module.css файлів
+declare module '*.module.css' {
+    const classes: { [key: string]: string };
+    export default classes;
+  }
+  
+  // для звичайних CSS-файлів, таких як index.css
+  declare module '*.css';
+  
